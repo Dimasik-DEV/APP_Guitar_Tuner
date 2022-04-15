cde74eecda32ec81e4814ce3d18c45a7ea249f49
Dimasik <dimchistov97@mail.ru>
2022-04-15 16:30:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#########################################
+# Мобильное приложение "Гитарный Тюнер" #
+#########################################
+
+import kivy
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class MyApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+
+MyApp().run()
